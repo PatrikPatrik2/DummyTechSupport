4ed9d73292ba9141e77cb1f7deb3d72f109160ae
PatrikPatrik2 <42711908+PatrikPatrik2@users.noreply.github.com>
2023-07-20 23:02:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,32 +38,14 @@ def analyse_email(email: Email):
     content = email.content
     persist_directory = Vector_folder_path
 
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0.2)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectordb.as_retriever(search_type="mmr", search_kwargs={"k":1000}))
 
-    
-    query = f"From the mail below, extract what shall be quoted using the Price List, select the items that yields the lowest total cost, only the quantities from the price list should be used and combined to fulfill the requested items You are working at the sales office at Uponor and your name in Pamela Anderson, write a friendly quotation to the person signing the mail. If there are items in the request that are not included in the price list note that the price for those items will be given later. Include the price for the items that could be found in the price list. If the request is using meters, convert it to ft before calculating the cost: {content} "
-    #system_message="price list: \n AquaPEX Pex pipe 1/2\": 1 usd/ft \n  AquaPEX Pex pipe 3/4\" : 1.3 usd/ft \n AquaPEX Pex pipe 3\" : 2 usd/ft \n Fitting 1/2\" : 2 usd/ft \n Fitting 3/4\" : 3 usd/ft \n Fitting 1\" : 4 usd/ft \n"
-    system_message="""
-
-"""
-    
-
-
-
-    
-    messages = [{"role": "user", "content": query}, {"role": "system", "content": system_message} ] 
-
-    response = openai.ChatCompletion.create(
-        #model="gpt-3.5-turbo-16k",
-        model="gpt-4",
-        temperature=0.5,
-        messages=messages
-    )
-
-    arguments = response.choices[0]["message"]["content"]
-    #response['choices'][0]['message']['content']})
     companyName ="z"
     cost = "0"
-    mail_reply = arguments
+    mail_reply = qa.run(content)
     
 
     return {
